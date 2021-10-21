18e7d46d84deb9c475e8a12d1809fa8413d2e4c0
berkaynayman <berkaynayman4@gmail.com>
2021-10-21 22:37:03 +0300

    .

--- DOM-ile-calismak/prompt-ile-kullanicidan-bilgi-almak/prompt-ile-kullanicidan-bilgi-almak.js
+++ DOM-ile-calismak/prompt-ile-kullanicidan-bilgi-almak/prompt-ile-kullanicidan-bilgi-almak.js
@@ -7,3 +7,5 @@ let fullName = prompt("Lütfen adinizi giriniz" , "")
 let h1 = document.querySelector('#fullName')
 
 h1.innerHTML = `${h1.innerHTML}  <small style="font-weight:bold;color:red;"> ${fullName} </small>`
+
+prompt("Boyunuzu giriniz", "Örnek: 166 cm");
