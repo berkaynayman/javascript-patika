c59c337883cd58ad9c5dd472b84023d224c83fcf
berkaynayman <berkaynayman4@gmail.com>
2021-10-22 23:34:43 +0300

    karsilastirma-operatorleri-matiksal-operatorler

---
+++ karar-yapilari-kosullar/karsilastirma-operatorleri-matiksal-operatorler.js
@@ -0,0 +1,24 @@
+let price = '100'
+let user  = 'berkay'
+
+// == eşitse
+console.log(price == 1)   // false
+console.log(price == 100) // true
+
+// === hem degeri hem turu eşit ise
+console.log(price === 1) // false
+console.log(price === 100) // false
+console.log(price === "100") // true
+
+// != eşit değilse
+console.log( user != 'nayman') // true
+console.log( user != 'berkay') // false
+
+// <=
+// >=
+// && ve
+// || veya
+// ! değil
+console.log("----------------");
+console.log(!2);
+console.log(!!2);
